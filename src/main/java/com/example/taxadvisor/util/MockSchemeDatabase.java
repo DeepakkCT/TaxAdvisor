d2ec47b84ea461f9ce6297aeb57b1936b64396cb
MohdAnwar-ct <mohd.anwar@clear.in>
2025-01-23 17:46:29 +0530

    Update MockSchemaDatabase

--- src/main/java/com/example/taxadvisor/util/MockSchemeDatabase.java
+++ src/main/java/com/example/taxadvisor/util/MockSchemeDatabase.java
@@ -14,24 +14,37 @@ public class MockSchemeDatabase {
         schemes.add(new InvestmentScheme("PPF", "Public Provident Fund", 150000, "80C",
                 "LOW", "LONG", 7.1));
         schemes.add(new InvestmentScheme("ELSS", "Equity Linked Savings Scheme", 150000, "80C",
-                "MEDIUM", "MEDIUM", 12.0));
+                "HIGH", "MEDIUM", 12.0));
         schemes.add(new InvestmentScheme("EPF", "Employee Provident Fund", 150000, "80C",
-                "LOW", "LONG", 8.5));
+                "LOW", "LONG", 8.25));
         schemes.add(new InvestmentScheme("ULIP", "Unit Linked Insurance Plan", 150000, "80C",
                 "MEDIUM", "LONG", 10.0));
         schemes.add(new InvestmentScheme("NSC", "National Savings Certificate", 150000, "80C",
-                "LOW", "MEDIUM", 6.8));
+                "LOW", "MEDIUM", 7.7));
         schemes.add(new InvestmentScheme("Tax-Saver FD", "5-Year Tax Saving Fixed Deposit", 150000, "80C",
-                "LOW", "MEDIUM", 6.5));
+                "LOW", "MEDIUM", 7.0));
         schemes.add(new InvestmentScheme("SCSS", "Senior Citizen Savings Scheme", 150000, "80C",
                 "LOW", "MEDIUM", 8.0));
         schemes.add(new InvestmentScheme("Life Insurance Premium", "Life Insurance Policies", 150000, "80C",
                 "LOW", "LONG", 5.0));
         schemes.add(new InvestmentScheme("Sukanya Samriddhi Yojana", "Girl Child Savings Scheme", 150000, "80C",
-                "LOW", "LONG", 8.0));
+                "LOW", "LONG", 8.2));
+
+        //80C -Tuitions fees for children
+        schemes.add(new InvestmentScheme("Tuition Fees", "Educational tuition fees", 150000, "80C",
+                "LOW", "SHORT", 0.0));
+
+        //80C- VPF (Voluntary Provident Fund)
+        schemes.add(new InvestmentScheme("VPF", "Voluntary Provident Fund", 150000, "80C",
+                "LOW", "LONG", 8.15));
+
+        //80D- Health Insurance
+        schemes.add(new InvestmentScheme("Health Insurance ", "Medical Insurance Premium", 25000, "80D",
+                "MEDIUM", "SHORT", 0.0));
+
 
         // 80CCD(1)
-        schemes.add(new InvestmentScheme("NPS Tier I", "National Pension System - Tier I", 50000, "80CCD(1)",
+        schemes.add(new InvestmentScheme("NPS Tier I", "National Pension System - Tier I", 50000, "80CCD(1B)",
                 "MEDIUM", "LONG", 10.0));
 
         // 80CCD(1B)
